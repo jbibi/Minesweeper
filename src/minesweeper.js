1cb829c985e6dcfea4d103e90e07e48859272c2f
jbibi <jbsjordan23@hotmail.com>
2017-07-23 20:19:44 -0400

    Refactored code

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,13 +1,49 @@
-const blankLine = '   |   |   ';
-const guessLine = ' 1 |   |   ';
-const bombLine = '   | B |   ';
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+    let board = [];
+    for (let i = 0; i < numberOfRows; i++) {
+        let row = [];
+        for (let j = 0; j < numberOfColumns; j++) {
+            row.push(' ');
+        }
+        board.push(row);
+    }
+    return board;
+}
 
-console.log('This is what an empty board would look like: ');
-console.log(blankLine);
-console.log(blankLine);
-console.log(blankLine);
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+    let board = [];
+    for (let i = 0; i < numberOfRows; i++) {
+        let row = [];
+        for (let j = 0; j < numberOfColumns; j++) {
+            row.push(null);
+        }
+        board.push(row);
+    }
+    let numberOfBombsPlaced = 0;
+    while (numberOfBombsPlaced < numberOfBombs) {
+        let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+        let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+        if (!(board[randomRowIndex][randomColumnIndex] === 'B')) {
+            board[randomRowIndex][randomColumnIndex] = 'B';
+            numberOfBombsPlaced++;
+        }
+    }
 
-console.log('This is what a board with a guess and a bomb on it would look like: ');
-console.log(guessLine);
-console.log(bombLine);
-console.log(blankLine);
+    return board;
+}
+
+const getNumberOfNeighborBombs = (bombBoard, rowIndex, columnIndex) => {
+
+}
+
+const printBoard = (board) => {
+    console.log(board.map(row => row.join(' | ')).join('\n'));
+}
+
+let playerBoard = generatePlayerBoard(3, 4);
+let bombBoard = generateBombBoard(3, 4, 5);
+
+console.log('Player Board: ');
+printBoard(playerBoard);
+console.log('Bomb Board: ');
+printBoard(bombBoard);
